2b0480d31ccd2bff1889a7fce1604bb86465b730
William Foster <21318458@bucks.ac.uk>
2018-10-11 11:22:38 +0100

    Music.cs file expanded

---
+++ mvcMusicWebsite/Models/Music.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcMusicWebsite.Models
+{
+    public class Music
+    {
+        [ScaffoldColumn(false)]
+        public int MusicID { get; set; }
+
+        [StringLength(50), Required]
+        public string BandName { get; set; }
+
+        [Required, Range(1, 15), DataType(DataType.MultilineText)]
+        public decimal Price { get; set; }
+
+        [StringLength(50), Required]
+        public string AlbumName { get; set; }
+
+        [StringLength(50), Required, Display(Name = "TracK Name")]
+        public string TrackName { get; set; }
+
+        [StringLength(50), Required]
+        public string ArtistName { get; set; }
+
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        //Place to find the image
+        [StringLength(128), DataType(DataType.Url)]
+        public string ImageURL { get; set; }
+
+        [StringLength(128)]
+        public string Rating { get; set; }
+
+        
+
+    }
+}
